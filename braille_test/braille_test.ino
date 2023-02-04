b184bd11d22812d6acacb9609d8f09c99b3a53f4
Amit Kumar <amitkh@gmail.com>
2023-02-04 14:20:12 -0500

    add braille fake dict

---
+++ braille_test/braille_test.ino
@@ -0,0 +1,48 @@
+#define A 0
+#define B 1
+#define C 2
+#define D 3
+#define E 4
+#define F 5
+#define G 6
+#define H 7
+#define I 8
+#define J 9
+#define K 10
+#define L 11
+#define M 12
+#define N 13
+#define O 14
+#define P 15
+#define Q 16
+#define R 17
+#define S 18
+#define T 19
+#define U 20
+#define V 21
+#define W 22
+#define X 23
+#define Y 24
+#define Z 25
+#define HASH 26
+#define ONE 27
+#define TWO 28
+#define THREE 29
+#define FOUR 30
+#define FIVE 31
+#define SIX 32
+#define SEVEN 33
+#define EIGHT 34
+#define NINE 35
+#define ZERO 36
+
+
+void setup() {
+  // put your setup code here, to run once:
+  const BRAILLE byte[][] = [[1], [1, 2], [1, 4], [1,4,5], [1,5], [1,2,4], [1,2,4,5], [1,2,5], [2,4], [2,4,5], [1,3], [1,2,3], [1,3,4], [1,3,4,5], [1,3,5], [1,2,3,4], [1,2,3,4,5], [1,2,3,5], [2,3,4], [2,3,4,5], [1,3,6], [1,2,3,6], [2,4,5,6], [1,3,4,6], [1,3,4,5,6], [1,3,5,6], [3,4,5,6], [1], [1,2], [1,4], [1,4,5], [1,5], [1,2,4], [1,2,4,5], [1,2,5], [2,4], [2,4,5]];
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

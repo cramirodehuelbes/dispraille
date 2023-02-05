42fbf215a0432abe4c286cd1e136e3237d5b5b01
Charlotte Ramiro <cramirodehuelbes@olin.edu>
2023-02-04 19:18:02 -0500

    Added braille_mapping.ino which converts letter codes to servo commands

---
+++ braille_mapping/braille_mapping.ino
@@ -0,0 +1,54 @@
+#include <Servo.h>
+
+// create six servos
+// 1  6
+// 2  5
+// 3  4
+
+Servo s1;
+Servo s2;
+Servo s3;
+Servo s4;
+Servo s5;  
+Servo s6;
+
+
+int pos = 0;    // variable to store the servo position
+
+byte BRAILLE [][6] = {{1}, {1, 2}, {1, 4}, {1,4,5}, {1,5}, {1,2,4}, {1,2,4,5}, {1,2,5}, {2,4}, {2,4,5}, {1,3}, {1,2,3}, {1,3,4}, {1,3,4,5}, {1,3,5}, {1,2,3,4}, {1,2,3,4,5}, {1,2,3,5}, {2,3,4}, {2,3,4,5}, {1,3,6}, {1,2,3,6}, {2,4,5,6}, {1,3,4,6}, {1,3,4,5,6}, {1,3,5,6}, {3,4,5,6}, {1}, {1,2}, {1,4}, {1,4,5}, {1,5}, {1,2,4}, {1,2,4,5}, {1,2,5}, {2,4}, {2,4,5}};
+Servo motors[6] = {s1, s2, s3, s4, s5, s6};
+
+void setup() {
+  s1.attach(3);  // attaches servo pins to servo objects
+  s2.attach(5);
+  s3.attach(6);
+  s4.attach(9);
+  s5.attach(10);
+  s6.attach(11);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  Serial.println("Insert letter");
+
+  while (Serial.available() == 0) {
+  }
+
+  int letter = Serial.parseInt();
+
+    for (pos = 0; pos <= 30; pos += 1) { // goes from 0 degrees to 180 degrees    
+      for (int i = 0; i<= sizeof(BRAILLE[letter]); i+=1){
+        // in steps of 1 degree
+        motors[BRAILLE[letter][i]-1].write(pos);              // tell servo to go to position in variable 'pos'
+        delay(15);                       // waits 15ms for the servo to reach the position
+      }}
+    
+    for (pos = 30; pos >= 0; pos -= 1) {     // goes from 180 degrees to 0 degrees
+      for (int i = 0; i<= sizeof(BRAILLE[letter]); i+=1){
+        // in steps of 1 degree
+        motors[BRAILLE[letter][i]-1].write(pos);              // tell servo to go to position in variable 'pos'
+        delay(15);                       // waits 15ms for the servo to reach the position
+    }}
+}
